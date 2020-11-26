45347632bd4f198b6eeddf827679ad803944caef
Simon Martinelli <simon@72.services>
2020-11-26 08:30:30 +0100

    Initial Commit

---
+++ src/main/java/io/seventytwo/failsafe/SpringBootFailsafeApplication.java
@@ -0,0 +1,13 @@
+package io.seventytwo.failsafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFailsafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFailsafeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/seventytwo/failsafe/SpringBootFailsafeApplicationIT.java
@@ -0,0 +1,13 @@
+package io.seventytwo.failsafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFailsafeApplicationIT {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
